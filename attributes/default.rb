24a8c7d0ee86dc59441dac4d74b9f49c23f255c4
Roger Delph <rdelph@expedia.com>
2015-09-18 19:33:48 -0700

    Initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,97 @@
+
+# descriptions are partially taken from the man page.
+
+# LOGGING OPTIONS
+# ------------------------------------------------------------------------
+
+# Drift log file tracks the drift of your system clock
+default['ntp']['drift_log_enabled'] = true
+default['ntp']['drift_log_location'] = '/var/lib/ntp/ntp.drift'
+
+# To seperate out the ntp log file from the default syslog set the 
+# attributes below
+default['ntp']['log_enabled'] = true
+default['ntp']['log_location'] = '/var/lib/ntp/ntp.log'
+
+
+# ACCESS CONTROLS
+# ------------------------------------------------------------------------
+# The following attributes will toggle various items for the 'restrict' 
+# configs
+
+# Prevents free access to the server
+default['ntp']['ac_default'] = true
+
+# Deny packets of all kind
+default['ntp']['ac_ignore'] = false
+
+# When an access violation occurs a kiss-o'-death (KoD) packet is sent.
+default['ntp']['ac_kod'] = true
+
+# Deny service if the packet spacing violate the lower limites in the
+# discard command
+default['ntp']['ac_limited'] = false
+default['ntp']['ac_discard_avg'] = 5
+default['ntp']['ac_discard_min'] = 2
+
+# Deny ntpq and ntpdc queries which attempt to modify the state of the
+# server.
+default['ntp']['ac_nomodify'] = true
+
+# Deny packets that would create a new association 
+default['ntp']['nopeer'] = true
+
+# Deny ntpq and ntpdc queries 
+default['ntp']['ac_noquery'] = true
+
+# Deny all packets except from ntpq and ntpdc
+default['ntp']['ac_noserve'] = false
+
+# Decline to provide message trap services for remote logging
+default['ntp']['ac_notrap'] = true
+
+# Deny service unless the packet is cryptographically authenticated
+default['ntp']['ac_notrust'] = false 
+
+# Deny packets that do not match the current NTP version
+default['ntp']['ac_version'] = false
+
+# Restrict access to loopback addresses only
+default['ntp']['ac_loopback_only'] = true
+
+
+# MODES
+# ------------------------------------------------------------------------
+# There are multiple ways to configure how ntp will sync time, please 
+# ensure that only one option is enabled
+
+# Server mode creates persistant association with the specified server.
+# The local clock can be synced to the remote clock
+default['ntp']['mode_server_enabled'] = true
+
+# Pool mode creates persistant association with mulitple remote servers.
+# The local clock can be synced to the remote clock
+default['ntp']['mode_pool_enabled'] = false
+
+# Peer Mode creates a symmetric-active association
+# Local and Remote clocks can be sycned to each other
+default['ntp']['mode_peer_enabled'] = false
+
+# What Servers do you want to use for the modes above
+# NOTE: If you prefer one of the servers enter prefer after the address.
+default['ntp']['mode_servers'] = ['0.pool.ntp.org','1.pool.ntp.org','2.pool.ntp.org','3.pool.ntp.org']
+
+# burst will send eight packets instead of one when the server is
+# reachable 
+default['ntp']['mode_burst'] = false
+
+# iburst will send eight packets instead of one when the server is not
+# reachable
+default['ntp']['mode_iburst'] = true
+
+
+# UNDISCIPLINED CLOCK
+# ------------------------------------------------------------------------
+
+# Enables the use of the local undisciplined clock
+default['ntp']['use_cmos'] = false
---
+++ recipes/default.rb
@@ -0,0 +1,45 @@
+#
+# Cookbook Name:: ntpd
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+# The Network Time Protocol is used to manage the system clock over a network. Computer 
+# clocks are not very accurate, so time will drift on unmanaged systems. If every machine 
+# on your network reliably reports the same time as every other machine, then it is much 
+# easier to correlate log messages in case of an attack. In addition, a number of 
+# cryptographic protocols (such as Kerberos) use timestamps to prevent certain types of 
+# attacks. If your network does not have synchronized time, these protocols may be unreliable 
+# or even unusable.
+
+yum_package 'ntp' do
+    action :install
+end 
+
+service 'ntpd' do
+    action [:enable, :start]
+end
+
+# ntpdate is a computer program used to quickly synchronize and set computers' date and 
+# time by querying a Network Time Protocol (NTP) server. 
+
+yum_package 'ntpdate' do
+    action :install
+end
+
+# The /etc/ntp.conf file contains the configuration for the ntpd service.  The template
+# (ntp.conf.erb) is used to set log file locations, restrict access to the service to
+# the local server, configure the name servers, etc.  If the file chanes the ntpd 
+# service has be be restarted.
+
+template '/etc/ntp.conf' do
+    source 'ntp.conf.erb'
+    owner 'root'
+    group 'root'
+    mode '0755'
+    notifies :restart, 'service[ntpd]'     
+end
