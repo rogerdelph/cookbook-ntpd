57b83680ae0dd7ba9a7ada69d128177ca50ebd52
Roger Delph <rdelph@expedia.com>
2015-09-22 06:29:22 -0700

    fixing issues with rubocop

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -14,7 +14,7 @@ describe 'ntpd::default' do
   it 'installs the ntp package' do
     expect(chef_run).to install_yum_package('ntp')
   end
-  
+
   it 'installs the ntpdate package' do
     expect(chef_run).to install_yum_package('ntpdate')
   end
@@ -22,12 +22,12 @@ describe 'ntpd::default' do
   it 'starts the ntpd service' do
     expect(chef_run).to start_service('ntpd')
   end
-  
+
   it 'creates a template with attributes' do
     expect(chef_run).to create_template('/etc/ntp.conf').with(
       owner: 'root',
       group: 'root',
-      mode: '0755',
+      mode: '0755'
     )
   end
 end
