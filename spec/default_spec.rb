3b2dfdfcf4399711010b083ad8554f230093f244
Roger Delph <rdelph@expedia.com>
2015-09-22 06:07:53 -0700

    more unit testing

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -11,7 +11,12 @@ describe 'ntpd::default' do
     ChefSpec::Runner.new(platform: 'centos', version: '7.0').converge described_recipe
   end
 
-    it 'installs the ntp package' do
-      expect(chef_run).to install_yum_package('ntp')
-    end
+  it 'installs the ntp package' do
+    expect(chef_run).to install_yum_package('ntp')
+  end
+  
+  it 'starts the ntpd service' do
+    expect(chef_run).to start_service('ntpd')
+  end
 end
+
